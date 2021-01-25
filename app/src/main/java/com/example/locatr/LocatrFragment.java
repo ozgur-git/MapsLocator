bc1f87347a6b450204d338e567bd74beac5fe9c9
ozgur-git <ozgursevik@gmail.com>
2021-01-25 07:21:34 +0300

    first commit

--- app/src/main/java/com/example/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/locatr/LocatrFragment.java
@@ -10,8 +10,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 public class LocatrFragment extends Fragment {
+
+    private GoogleApiClient mClient;
 
 
     private ImageView mImageView;
@@ -24,6 +28,9 @@ public class LocatrFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        mClient=new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Nullable
@@ -38,7 +45,21 @@ public class LocatrFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_locatr,menu);
+
+        MenuItem searchItem=menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        mClient.disconnect();
+    }
 }
